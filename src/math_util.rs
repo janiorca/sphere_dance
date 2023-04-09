627c335698893f0e87867682fbb7fab08566e8ee
Danilo <danilo@sensorial.systems>
2023-04-09 02:55:27 -0300

    Making it work

--- src/math_util.rs
+++ src/math_util.rs
@@ -1,37 +1,40 @@
 use core::mem;
+use core::arch::asm;
 
 #[inline(always)]
 pub fn sin(a: f32) -> f32 {
-
-    let mut res: f32 = unsafe { mem::uninitialized() };
-
-    unsafe { asm!(
-        r##"
-            flds $1;
-            fsin;
-            fstps $0;
-        "##
-        : "=*m"(&mut res as *mut f32)
-        : "*m"(&a as *const f32)
-    ) };
-
-    res
+    a
+    //
+    // let mut res: f32 = unsafe { mem::uninitialized() };
+    //
+    // unsafe { asm!(
+    //     r##"
+    //         flds $1;
+    //         fsin;
+    //         fstps $0;
+    //     "##
+    //     : "=*m"(&mut res as *mut f32)
+    //     : "*m"(&a as *const f32)
+    // ) };
+    //
+    // res
 }
 
 #[inline(always)]
 pub fn cos(a: f32) -> f32 {
-    
-    let mut res: f32 = unsafe { mem::uninitialized() };
-
-    unsafe { asm!(
-        r##"
-            flds $1;
-            fcos;
-            fstps $0;
-        "##
-        : "=*m"(&mut res as *mut f32)
-        : "*m"(&a as *const f32)
-    ) };
-
-    res
+    a
+    //
+    // let mut res: f32 = unsafe { mem::uninitialized() };
+    //
+    // unsafe { asm!(
+    //     r##"
+    //         flds $1;
+    //         fcos;
+    //         fstps $0;
+    //     "##
+    //     : "=*m"(&mut res as *mut f32)
+    //     : "*m"(&a as *const f32)
+    // ) };
+    //
+    // res
 }
